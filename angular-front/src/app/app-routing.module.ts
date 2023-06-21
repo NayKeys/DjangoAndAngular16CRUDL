4588528623cd086a8dc8127848a4c7971f12532a
SUSSY <naykeysnet@gmail.com>
2023-06-21 12:06:48 +0200

    Fixing anular routing

--- angular-front/src/app/app-routing.module.ts
+++ angular-front/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentComponent } from './student/student.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/student', pathMatch: 'full' },
+  { path: 'student', component: StudentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- sussy_crudproject/urls.py
+++ sussy_crudproject/urls.py
@@ -21,5 +21,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
   path('admin/', admin.site.urls),
-  path('student/crud', TemplateView.as_view(template_name='./static/index.html')),
+  path('', TemplateView.as_view(template_name='index.html')),
 ]
