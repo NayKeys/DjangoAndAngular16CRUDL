6b2e7ee88b0e30a69ca37826c87b7cd542141626
sussy-code <naykeysnet@gmail.com>
2023-07-03 10:34:27 +0200

    Creating new view system

--- datahub/config.py
+++ datahub/config.py
@@ -3,3 +3,25 @@
   
   Having sql dictionnary with key:vue_name pointing to sql table name -> will be used in the hub.py
 """
+
+VIEW_LIST = [
+  {
+    "vue_name": "sql_view_1",
+    "method": "sql",
+    "table_name": "students_app_student",
+    "database_url": "studentsDB.sqlite3",
+    "identifier_name": "id",
+  },
+  {
+    "vue_name": "sql_view_2",
+    "method": "sql",
+    "table_name": "students_app_student",
+    "database_url": "studentsDB.sqlite3",
+    "identifier_name": "id",
+  },
+  {
+    "vue_name": "view3",
+    "method": "csv",
+    "database_url": "view1.csv",
+  },
+]
--- datahub/pipelines/hub.py
+++ datahub/pipelines/hub.py
@@ -21,83 +21,85 @@ class ApiResponse:
       return JsonResponse({"status": self.status, "message": self.message}, status=self.status)
     return JsonResponse({"status": self.status, "message": self.message, "data": self.data}, status=self.status)
 
-SQL_TABLES = [
-  {
-    "table_name": "students_app_student",
-    "database_url": "studentsDB.sqlite3",
-    "identifier_name": "id",
-  }
-]
-CSV_FILES = ["view1.csv", "view2.csv"]
-LDAP_ENDPOINT = ["dc=org1", "dc=org2"]  # This is just an example I don't have any idea of what I just typed here
-
 import datahub.pipelines.SQLPipeline as sql_pipeline
 import datahub.pipelines.CSVPipeline as csv_pipeline
 import datahub.pipelines.LDAPPipeline as ldap_pipeline
+import datahub.config as config
+
+SQL_VIEWS = []
+CSV_VIEWS = []
+LDAP_VIEWS = []
+for view in config.VIEW_LIST:
+  if view['method'] == 'sql':
+    SQL_VIEWS.append(view)
+  elif view['method'] == 'csv':
+    CSV_VIEWS.append(view)
+  elif view['method'] == 'ldap':
+    LDAP_VIEWS.append(view)
 
 def fetch_all(view_name: str):
   if (view_name == "sql_view_1"):
-    dict_list = sql_pipeline.fetch_all(SQL_TABLES[0])  # Passing table name as parameter
+    dict_list = sql_pipeline.fetch_all(SQL_VIEWS[0])  # Passing table name as parameter
   elif (view_name == "csv_view_1"):
-    dict_list = csv_pipeline.fetch_all(CSV_FILES[0])  # Choosing which csv file to fetch
+    dict_list = csv_pipeline.fetch_all(CSV_VIEWS[0])  # Choosing which csv file to fetch
   elif (view_name == "csv_view_2"):
-    dict_list = csv_pipeline.fetch_all(CSV_FILES[1])
+    dict_list = csv_pipeline.fetch_all(CSV_VIEWS[1])
   elif (view_name == "ldap_view_1"):
-    dict_list = ldap_pipeline.fetch_all(LDAP_ENDPOINT[0])
+    dict_list = ldap_pipeline.fetch_all(LDAP_VIEWS[0])
   elif (view_name == "ldap_view_1"):
-    dict_list = ldap_pipeline.fetch_all(LDAP_ENDPOINT[1])
+    dict_list = ldap_pipeline.fetch_all(LDAP_VIEWS[1])
   return dict_list
 
 def fetch(view_name: str, identifier: str):
   if (view_name == "sql_view_1"):
-    dict_list = sql_pipeline.fetch(SQL_TABLES[0], identifier)
+    dict_list = sql_pipeline.fetch(SQL_VIEWS[0], identifier)
   elif (view_name == "csv_view_1"):
-    dict_list = csv_pipeline.fetch(CSV_FILES[0], identifier)
+    dict_list = csv_pipeline.fetch(CSV_VIEWS[0], identifier)
   elif (view_name == "csv_view_2"):
-    dict_list = csv_pipeline.fetch(CSV_FILES[1], identifier)
+    dict_list = csv_pipeline.fetch(CSV_VIEWS[1], identifier)
   elif (view_name == "ldap_view_1"):
-    dict_list = ldap_pipeline.fetch(LDAP_ENDPOINT[0], identifier)
+    dict_list = ldap_pipeline.fetch(LDAP_VIEWS[0], identifier)
   elif (view_name == "ldap_view_1"):
-    dict_list = ldap_pipeline.fetch(LDAP_ENDPOINT[1], identifier)
+    dict_list = ldap_pipeline.fetch(LDAP_VIEWS[1], identifier)
   return dict_list
 
 def delete(view_name: str, identifier: str):
   if (view_name == "sql_view_1"):
-    response = sql_pipeline.delete(SQL_TABLES[0], identifier)
+    response = sql_pipeline.delete(SQL_VIEWS[0], identifier)
   elif (view_name == "csv_view_1"):
-    response = csv_pipeline.delete(CSV_FILES[0], identifier)
+    response = csv_pipeline.delete(CSV_VIEWS[0], identifier)
   elif (view_name == "csv_view_2"):
-    response = csv_pipeline.delete(CSV_FILES[1], identifier)
+    response = csv_pipeline.delete(CSV_VIEWS[1], identifier)
   elif (view_name == "ldap_view_1"):
-    response = ldap_pipeline.delete(LDAP_ENDPOINT[0], identifier)
+    response = ldap_pipeline.delete(LDAP_VIEWS[0], identifier)
   elif (view_name == "ldap_view_1"):
-    response = ldap_pipeline.delete(LDAP_ENDPOINT[1], identifier)
+    response = ldap_pipeline.delete(LDAP_VIEWS[1], identifier)
   return response
 
 def insert(view_name: str, data: dict):
   if (view_name == "sql_view_1"):
-    response = sql_pipeline.insert(SQL_TABLES[0], data)
+    response = sql_pipeline.insert(SQL_VIEWS[0], data)
   elif (view_name == "csv_view_1"):
-    response = csv_pipeline.insert(CSV_FILES[0], data)
+    response = csv_pipeline.insert(CSV_VIEWS[0], data)
   elif (view_name == "csv_view_2"):
-    response = csv_pipeline.insert(CSV_FILES[1], data)
+    response = csv_pipeline.insert(CSV_VIEWS[1], data)
   elif (view_name == "ldap_view_1"):
-    response = ldap_pipeline.insert(LDAP_ENDPOINT[0], data)
+    response = ldap_pipeline.insert(LDAP_VIEWS[0], data)
   elif (view_name == "ldap_view_1"):
-    response = ldap_pipeline.insert(LDAP_ENDPOINT[1], data)
+    response = ldap_pipeline.insert(LDAP_VIEWS[1], data)
   return response
 
 def update(view_name: str, identifier: str, data: dict):
   if (view_name == "sql_view_1"):
-    response = sql_pipeline.update(SQL_TABLES[0], identifier, data)
+    response = sql_pipeline.update(SQL_VIEWS[0], identifier, data)
   elif (view_name == "csv_view_1"):
-    response = csv_pipeline.update(CSV_FILES[0], identifier, data)
+    response = csv_pipeline.update(CSV_VIEWS[0], identifier, data)
   elif (view_name == "csv_view_2"):
-    response = csv_pipeline.update(CSV_FILES[1], identifier, data)
+    response = csv_pipeline.update(CSV_VIEWS[1], identifier, data)
   elif (view_name == "ldap_view_1"):
-    response = ldap_pipeline.update(LDAP_ENDPOINT[0], identifier, data)
+    response = ldap_pipeline.update(LDAP_VIEWS[0], identifier, data)
   elif (view_name == "ldap_view_1"):
-    response = ldap_pipeline.update(LDAP_ENDPOINT[1], identifier, data)
+    response = ldap_pipeline.update(LDAP_VIEWS[1], identifier, data)
   return response
 
 def check_permission_create(data, user):
