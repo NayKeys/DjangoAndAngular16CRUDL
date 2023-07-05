a757080460b860772dc160a4d22f01627519daeb
sussy-code <naykeysnet@gmail.com>
2023-07-05 14:26:40 +0200

    Styling datatables

--- test-svelte/src/Table.svelte
+++ test-svelte/src/Table.svelte
@@ -1,185 +1,274 @@
 <script lang="ts">
+  /*
+  SEE: https://github.com/dhobi/datatables.colResize for column resizing implementation
+  */
+  import { Table as StrapTable } from 'sveltestrap'
+  import j from 'jquery'
+	import { onMount, tick } from 'svelte'
+	import DataTable from 'datatables.net-dt'
+  import 'datatables.net-buttons-dt';
+  import 'datatables.net-fixedheader-dt';
+  import 'datatables.net-keytable-dt';
+  import 'datatables.net-responsive-dt';
+  import 'datatables.net-scroller-dt';
+  import 'datatables.net-searchbuilder-dt';
+  import 'datatables.net-select-dt';
+
+  
   import type { RowKeys, RowValues } from './main'
   export let columnNames:RowKeys  // Prop
-  export let table: RowValues[] // Prop
+  export let tableData: RowValues[] // Prop
   export let newRow: RowValues // Prop
   export let addRow: RowValues // Prop
   export let updateRow: RowValues // Prop
   export let deleteRow: RowValues // Prop
-
-  import DataTable, { Head, Body, Row, Cell, Label, SortValue, } from '@smui/data-table';
-  import IconButton from '@smui/icon-button';
-
-  type User = {
-    id: number;
-    name: string;
-    username: string;
-    email: string;
-    website: string;
-  };
-  let items: User[] = [];
-  let sort: keyof User = 'id';
-  let sortDirection: Lowercase<keyof typeof SortValue> = 'ascending';
-
-  if (typeof fetch !== 'undefined') {
-    fetch(
-      'https://gist.githubusercontent.com/hperrin/e24a4ebd9afdf2a8c283338ae5160a62/raw/dcbf8e6382db49b0dcab70b22f56b1cc444f26d4/users.json'
-    )
-      .then((response) => response.json())
-      .then((json) => (items = json));
-  }
-
-  function handleSort() {
-    items.sort((a, b) => {
-      const [aVal, bVal] = [a[sort], b[sort]][
-        sortDirection === 'ascending' ? 'slice' : 'reverse'
-      ]();
-      if (typeof aVal === 'string' && typeof bVal === 'string') {
-        return aVal.localeCompare(bVal);
-      }
-      return Number(aVal) - Number(bVal);
+  
+  onMount(async () => {
+    let table = new DataTable('#table', {
+      responsive: true,
+      search: {
+        return: false
+      },
+      paging: true,
+      ordering: true,
+      scrollY: "400",
+      select: true,
+      pageLength: 50,
     });
-    items = items;
-  }
+    j('#table_wrapper')[0].prepend(j('.table-buttons')[0]);
+  });
+  // bind:this={table}
 </script>
-<DataTable
-  sortable
-  bind:sort
-  bind:sortDirection
-  on:SMUIDataTable:sorted={handleSort}
-  table$aria-label="User list"
-  style="width: 100%;"
->
-  <Head>
-    <Row>
-      <!--
-        Note: whatever you supply to "columnId" is
-        appended with "-status-label" and used as an ID
-        for the hidden label that describes the sort
-        status to screen readers.
- 
-        You can localize those labels with the
-        "sortAscendingAriaLabel" and
-        "sortDescendingAriaLabel" props on the DataTable.
-      -->
-      <Cell numeric columnId="id">
-        <!-- For numeric columns, icon comes first. -->
-        <IconButton class="material-icons">arrow_upward</IconButton>
-        <Label>ID</Label>
-      </Cell>
-      <Cell columnId="name" style="width: 100%;">
-        <Label>Name</Label>
-        <!-- For non-numeric columns, icon comes second. -->
-        <IconButton class="material-icons">arrow_upward</IconButton>
-      </Cell>
-      <Cell columnId="username">
-        <Label>Username</Label>
-        <IconButton class="material-icons">arrow_upward</IconButton>
-      </Cell>
-      <Cell columnId="email">
-        <Label>Email</Label>
-        <IconButton class="material-icons">arrow_upward</IconButton>
-      </Cell>
-      <!-- You can turn off sorting for a column. -->
-      <Cell sortable={false}>Website</Cell>
-    </Row>
-  </Head>
-  <Body>
-    {#each items as item (item.id)}
-      <Row>
-        <Cell numeric>{item.id}</Cell>
-        <Cell>{item.name}</Cell>
-        <Cell>{item.username}</Cell>
-        <Cell>{item.email}</Cell>
-        <Cell>{item.website}</Cell>
-      </Row>
-    {/each}
-  </Body>
-</DataTable>
-<table class="table">
-  <tr class="row-1">
-    {#each columnNames as column}
-    <th class="cell">
-      <div class="content">
-        <div class="text lexenddeca-semi-bold-oslo-gray-20px  ">
-          {column}
-        </div>
-      </div>
-    </th>
-    {/each}
-  </tr>
-  {#each table as row}
-  <tr class="row">
-    {#each row as cell}
-    <th class="cell">
-      <div class="content">
-        <div class="text  lexenddeca-normal-geyser-24px">
-          {cell}
-        </div>
-      </div>
-    </th>
-    {/each}
-  </tr>
-  {/each}
-</table>
+
+<svelte:head>
+	<!-- <link rel="stylesheet" href="//cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css" /> -->
+</svelte:head>
+
+<div class="table-frame screen">
+  <div class="frame-31">
+    <h1 class="vue-tudiants-1re-anne valign-text-middle">Vue étudiants 1ère année</h1>
+    <div class="top-seperator"></div>
+  </div>
+  <div class="table-buttons">
+    <div class="search">
+      <img class="vector" src="https://anima-uploads.s3.amazonaws.com/projects/63f7f6d546da9210f99dd5aa/releases/64a3f5479ef0ce55861f0160/img/vector.svg" alt="Vector" />
+      <div class="search-1 valign-text-middle lexenddeca-normal-oslo-gray-24px">Search</div>
+    </div>
+    <div class="filter">
+      <img class="vector-1" src="https://anima-uploads.s3.amazonaws.com/projects/63f7f6d546da9210f99dd5aa/releases/64a3f5479ef0ce55861f0160/img/vector-1.svg" alt="Vector" />
+      <div class="filter-1 valign-text-middle lexenddeca-normal-oslo-gray-24px">Filter</div>
+      <div class="frame-31-1"><div class="number valign-text-middle lexenddeca-normal-oslo-gray-24px">7</div></div>
+    </div>
+    <div class="sort">
+      <img class="vector-2" src="https://anima-uploads.s3.amazonaws.com/projects/63f7f6d546da9210f99dd5aa/releases/64a3f5479ef0ce55861f0160/img/vector-2.svg" alt="Vector" />
+      <div class="sort-1 valign-text-middle lexenddeca-normal-oslo-gray-24px">Sort</div>
+    </div>
+  </div>
+  <table id="table" class="dataTable display table hover order-column row-border stripe">
+    <thead>
+      <tr class="row-1">
+        {#each columnNames as column}
+        <th class="cell">
+          <div class="content">
+            <div class="text lexenddeca-semi-bold-oslo-gray-20px  ">
+              {column}
+            </div>
+          </div>
+        </th>
+        {/each}
+      </tr>
+    </thead>
+    <tbody>
+      {#each tableData as row}
+      <tr class="row">
+        {#each row as cell}
+        <th class="cell">
+          <div class="content">
+            <div class="text  lexenddeca-normal-geyser-24px">
+              {cell}
+            </div>
+          </div>
+        </th>
+        {/each}
+      </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
 
 <style>
   .table {
-    align-items: flex-start;
-    align-self: stretch;
-    border-radius: 4px;
-    display: flex;
-    flex-direction: column;
     overflow: hidden;
-    position: relative;
   }
   .content {
-    align-items: flex-start;
-    align-self: stretch;
-    display: flex;
     padding: 3px 12px;
-    position: relative;
   }
   .text {
     text-align: start;
     flex: 1;
     letter-spacing: 0;
-    line-height: 31.2px;
+    line-height: 28px;
     margin-top: -1px;
     position: relative;
     white-space: nowrap;
   }
   .row-1 {
-    align-items: flex-start;
-    align-self: stretch;
-    background-color: #ffffff00;
-    display: flex;
     height: 51px;
-    overflow: hidden;
-    position: relative;
   }
   .row {
-    align-items: flex-start;
-    align-self: stretch;
-    background-color: #ffffff00;
-    display: flex;
     height: 46px;
-    overflow: hidden;
-    position: relative;
   }
   .cell {
-    align-items: flex-start;
-    align-self: stretch;
-    background-color: var(--shark);
-    border: 0px none;
-    display: flex;
-    flex-direction: column;
-    position: relative;
-    width: 250px;
-    overflow: hidden;
-    border-top-style: solid;
+    /* border-top-style: solid;
     border-top-width: 1px;
     border-right-style: solid;
-    border-right-width: 1px;
+    border-right-width: 1px; */
+  }
+  .table-frame {
+    align-items: flex-start;
+    background-color: var(--shark);
+    border: 1px none;
+    display: flex;
+    flex-direction: column;
+    gap: 50px;
+    justify-content: center;
+    min-width: 1164.44px;
+    padding: 25px;
+    position: relative;
+  }
+  .table-buttons {
+    align-items: center;
+    align-self: stretch;
+    display: flex;
+    gap: 37px;
+    position: relative;
+  }
+  .sort {
+    align-items: center;
+    background-color: var(--eerie-black);
+    border: 2px solid;
+    border-color: var(--bright-gray);
+    border-radius: 5px;
+    display: flex;
+    gap: 8px;
+    justify-content: center;
+    padding: 12px 16px;
+    position: relative;
+    width: fit-content;
+  }
+  .vector-2 {
+    height: 24.98px;
+    margin-left: -2.5px;
+    min-width: 40.96px;
+    position: relative;
+  }
+  .valign-text-middle {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  .sort-1 {
+    height: 31px;
+    letter-spacing: 0;
+    line-height: normal;
+    margin-top: -2px;
+    position: relative;
+    width: 53px;
+  }
+  .search {
+    align-items: center;
+    background-color: var(--eerie-black);
+    border: 2px solid;
+    border-color: var(--bright-gray);
+    border-radius: 5px;
+    display: flex;
+    gap: 12px;
+    padding: 12px 15px;
+    position: relative;
+    width: 748px;
+  }
+  .vector {
+    height: 31px;
+    min-width: 31px;
+    position: relative;
+  }
+  .search-1 {
+    height: 30.61px;
+    letter-spacing: 0;
+    line-height: normal;
+    margin-top: -1.8px;
+    position: relative;
+    width: 89.27px;
+  }
+  .filter {
+    align-items: center;
+    background-color: var(--eerie-black);
+    border: 2px solid;
+    border-color: var(--bright-gray);
+    border-radius: 5px;
+    display: flex;
+    gap: 8px;
+    padding: 8px;
+    position: relative;
+    width: fit-content;
+  }
+  .filter-1 {
+    height: 31px;
+    letter-spacing: 0;
+    line-height: normal;
+    position: relative;
+    width: 67px;
+  }
+  .frame-31-1 {
+    align-items: center;
+    background-color: #2a944780;
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    overflow: hidden;
+    position: relative;
+    width: fit-content;
+  }
+  .number {
+    height: 31px;
+    letter-spacing: 0;
+    line-height: normal;
+    margin-top: -1px;
+    position: relative;
+    text-align: center;
+    width: 31px;
+  }
+  .vector-1 {
+    height: 38.1px;
+    min-width: 33.48px;
+    position: relative;
+  }
+  .frame-31 {
+    align-items: flex-start;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    justify-content: center;
+    position: relative;
+    width: fit-content;
+  }
+  .vue-tudiants-1re-anne {
+    color: #d7dde6;
+    font-family: var(--font-family-lexend_deca);
+    font-size: var(--font-size-l3);
+    font-weight: 400;
+    height: 31px;
+    letter-spacing: 0;
+    line-height: normal;
+    margin-top: -1px;
+    position: relative;
+    width: 394px;
+  }
+  .top-seperator {
+    background-color: var(--bright-gray);
+    border-radius: 10px;
+    height: 4px;
+    min-width: 1114px;
+    position: relative;
   }
 </style>
