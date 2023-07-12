4354c2abbeb444f55e893f8b6c5cafe504aa82b8
sussy-code <naykeysnet@gmail.com>
2023-07-12 13:09:54 +0200

    production url

--- sussy_crudproject/settings.py
+++ sussy_crudproject/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
   BASE_DIR / 'static',
@@ -181,12 +183,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
